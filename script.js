0f4756fe26b07363312093b97e7fdf97e7d18728
Lamadrid-Mike <mikealamadrid@hotmail.com>
2024-02-25 20:04:37 -0500

    cube almost done

---
+++ script.js
@@ -0,0 +1,95 @@
+let xAngle = 0;
+let yAngle = 0;
+
+let cube = document.getElementById("cube");
+let outerLayerCube = document.getElementById("outer__layer-cube");
+let earth = document.getElementById("earth");
+let frontSideCube = document.getElementById("front");
+let leftSideCube = document.getElementById("left");
+let backSideCube = document.getElementById("back");
+let rightSideCube = document.getElementById("right");
+let topSideCube = document.getElementById("top");
+let bottomSideCube = document.getElementById("bottom");
+let topImageFront = document.getElementById("top__front-image");
+
+const cubeRotation = function (xAngle, yAngle) {
+  cube.style.transform = `rotateX(${xAngle}deg) rotateY(${yAngle}deg)`;
+};
+
+//change background function!
+const imageChanger = () => {
+  earth.style.background
+    ? (earth.style.background = "")
+    : (earth.style.background = `url("/images/good-earth.jpg")`);
+};
+
+//cube effect will be here!
+const timeoutEffect = (seconds) => {
+  seconds = seconds * 1000;
+  return new Promise((resolve) => {
+    setTimeout(resolve, seconds);
+  });
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  timeoutEffect(3)
+    .then(() => {
+      yAngle += 90;
+      cubeRotation(xAngle, yAngle);
+    })
+    .then(() => {
+      leftSideCube.style.opacity = 1;
+      leftSideCube.classList.add("left-effect");
+      return timeoutEffect(3);
+    })
+    .then(() => {
+      yAngle += 90;
+      cubeRotation(xAngle, yAngle);
+    })
+    .then(() => {
+      backSideCube.style.opacity = 1;
+      backSideCube.classList.add("back-effect");
+      return timeoutEffect(3);
+    })
+    .then(() => {
+      yAngle += 90;
+      cubeRotation(xAngle, yAngle);
+    })
+    .then(() => {
+      rightSideCube.style.opacity = 1;
+      rightSideCube.classList.add("right-effect");
+      return timeoutEffect(3);
+    })
+    .then(() => {
+      xAngle -= 90;
+      yAngle += 90;
+      cubeRotation(xAngle, yAngle);
+    })
+    .then(() => {
+      topSideCube.style.opacity = 1;
+      topSideCube.classList.add("top-effect");
+      return timeoutEffect(3);
+    })
+    .then(() => {
+      xAngle += 180;
+      cubeRotation(xAngle, yAngle);
+    })
+    .then(() => {
+      bottomSideCube.style.opacity = 1;
+      bottomSideCube.classList.add("bottom-effect");
+      return timeoutEffect(3);
+    })
+    .then(() => {
+      xAngle += -70;
+      yAngle += 0;
+      cubeRotation(xAngle, yAngle);
+      topImageFront.style.transform = "rotate(180deg)";
+      return timeoutEffect(1);
+    })
+    .then(() => {
+      outerLayerCube.classList.add("cube-rotation");
+      earth.style.animation =
+        "earth-rotate 5s linear infinite, background-spin 15s infinite";
+      return timeoutEffect(5);
+    });
+});
