032d7ca7accfb3ea2bd94aeba59b7103268c48ff
Lamadrid-Mike <mikealamadrid@hotmail.com>
2024-04-05 20:46:38 -0400

    fixing loading issues

--- script.js
+++ script.js
@@ -56,108 +56,111 @@ const hideSidesCube = function () {
 //true will be good to bad gif, and good image background
 //false will be bad to good gif, and bad image background
 
-const playCube = function () {
-  earth.style.animation = "background-spin 30s linear infinite";
-  earth.style.opacity = 1;
-  cubeCycle
-    ? (earth.style.background = `url("/images/bad-to-good-earth.gif") 0px 0px / cover`)
-    : (earth.style.background = `url("/images/good-to-bad-earth.gif") 0px 0px / cover`);
-  timeoutEffect(2)
-    .then(() => {
-      frontSideCube.style.opacity = 1;
-      frontSideCube.classList.add("front-effect");
-      return timeoutEffect(2);
-    })
-    .then(() => {
-      yAngle += 90;
-      cubeRotation(xAngle, yAngle);
-    })
-    .then(() => {
-      leftSideCube.style.opacity = 1;
-      leftSideCube.classList.add("left-effect");
-      return timeoutEffect(2);
-    })
-    .then(() => {
-      yAngle += 90;
-      cubeRotation(xAngle, yAngle);
-    })
-    .then(() => {
-      backSideCube.style.opacity = 1;
-      backSideCube.classList.add("back-effect");
-      return timeoutEffect(2);
-    })
-    .then(() => {
-      yAngle += 90;
-      cubeRotation(xAngle, yAngle);
-    })
-    .then(() => {
-      rightSideCube.style.opacity = 1;
-      rightSideCube.classList.add("right-effect");
-      return timeoutEffect(2);
-    })
-    .then(() => {
-      xAngle -= 90;
-      yAngle += 90;
-      cubeRotation(xAngle, yAngle);
-    })
-    .then(() => {
-      topSideCube.style.opacity = 1;
-      topSideCube.classList.add("top-effect");
-      return timeoutEffect(2);
-    })
-    .then(() => {
-      xAngle += 180;
-      cubeRotation(xAngle, yAngle);
-    })
-    .then(() => {
-      bottomSideCube.style.opacity = 1;
-      bottomSideCube.classList.add("bottom-effect");
-      return timeoutEffect(2);
-    })
-    .then(() => {
-      xAngle += -60;
-      yAngle += 0;
-      cubeRotation(xAngle, yAngle);
-      topImageFront.style.transform = "rotate(180deg)";
-      return timeoutEffect(1);
-    })
-    .then(() => {
-      cubeCycle
-        ? (earth.style.background = `url("/images/good-earth.jpg") 0px 0px / cover`)
-        : (earth.style.background = `url("/images/bad-earth.jpg") 0px 0px / cover`);
-      outerLayerCube.classList.add("cube-rotation");
-      earth.style.animation =
-        "earth-rotate 5s linear infinite, background-spin 30s linear infinite";
-      return timeoutEffect(5);
-    })
-    .then(() => {
-      cube.classList.add("active-hover");
-      return timeoutEffect(9);
-    })
-    .then(() => {
-      earth.style.opacity = 0;
-      earth.style.animation = "background-spin 30s linear infinite";
-      hideSidesCube();
-      xAngle = 0;
-      yAngle = 0;
-      cubeRotation(xAngle, yAngle);
-      cube.classList.remove("active-hover");
-      outerLayerCube.classList.remove("cube-rotation");
-      return timeoutEffect(1);
-    })
-    .then(() => {
-      changeCubeImages();
-      cubeCycle = !cubeCycle;
-      topImageFront.style.transform = "";
-      cubeText.style.opacity = 1;
-      return timeoutEffect(3);
-    })
-    .then(() => {
-      earth.style.animation = "";
-      cubeText.style.opacity = 0;
-      return timeoutEffect(1);
-    })
-    .then(() => {
-      playCube();
-    });
-};
+addEventListener("DOMContentLoaded", function () {
+  const playCube = function () {
+    cubeCycle
+      ? (earth.style.background = `url("/images/bad-to-good-earth.gif") 0px 0px / cover`)
+      : (earth.style.background = `url("/images/good-to-bad-earth.gif") 0px 0px / cover`);
+    earth.style.animation = "background-spin 30s linear infinite";
+    earth.style.opacity = 1;
+    timeoutEffect(2)
+      .then(() => {
+        frontSideCube.style.opacity = 1;
+        frontSideCube.classList.add("front-effect");
+        return timeoutEffect(2);
+      })
+      .then(() => {
+        yAngle += 90;
+        cubeRotation(xAngle, yAngle);
+      })
+      .then(() => {
+        leftSideCube.style.opacity = 1;
+        leftSideCube.classList.add("left-effect");
+        return timeoutEffect(2);
+      })
+      .then(() => {
+        yAngle += 90;
+        cubeRotation(xAngle, yAngle);
+      })
+      .then(() => {
+        backSideCube.style.opacity = 1;
+        backSideCube.classList.add("back-effect");
+        return timeoutEffect(2);
+      })
+      .then(() => {
+        yAngle += 90;
+        cubeRotation(xAngle, yAngle);
+      })
+      .then(() => {
+        rightSideCube.style.opacity = 1;
+        rightSideCube.classList.add("right-effect");
+        return timeoutEffect(2);
+      })
+      .then(() => {
+        xAngle -= 90;
+        yAngle += 90;
+        cubeRotation(xAngle, yAngle);
+      })
+      .then(() => {
+        topSideCube.style.opacity = 1;
+        topSideCube.classList.add("top-effect");
+        return timeoutEffect(2);
+      })
+      .then(() => {
+        xAngle += 180;
+        cubeRotation(xAngle, yAngle);
+      })
+      .then(() => {
+        bottomSideCube.style.opacity = 1;
+        bottomSideCube.classList.add("bottom-effect");
+        return timeoutEffect(2);
+      })
+      .then(() => {
+        xAngle += -60;
+        yAngle += 0;
+        cubeRotation(xAngle, yAngle);
+        topImageFront.style.transform = "rotate(180deg)";
+        return timeoutEffect(1);
+      })
+      .then(() => {
+        cubeCycle
+          ? (earth.style.background = `url("/images/good-earth.jpg") 0px 0px / cover`)
+          : (earth.style.background = `url("/images/bad-earth.jpg") 0px 0px / cover`);
+        outerLayerCube.classList.add("cube-rotation");
+        earth.style.animation =
+          "earth-rotate 5s linear infinite, background-spin 30s linear infinite";
+        return timeoutEffect(5);
+      })
+      .then(() => {
+        cube.classList.add("active-hover");
+        return timeoutEffect(9);
+      })
+      .then(() => {
+        earth.style.opacity = 0;
+        earth.style.animation = "background-spin 30s linear infinite";
+        hideSidesCube();
+        xAngle = 0;
+        yAngle = 0;
+        cubeRotation(xAngle, yAngle);
+        cube.classList.remove("active-hover");
+        outerLayerCube.classList.remove("cube-rotation");
+        return timeoutEffect(1);
+      })
+      .then(() => {
+        changeCubeImages();
+        cubeCycle = !cubeCycle;
+        topImageFront.style.transform = "";
+        cubeText.style.opacity = 1;
+        return timeoutEffect(3);
+      })
+      .then(() => {
+        earth.style.animation = "";
+        cubeText.style.opacity = 0;
+        return timeoutEffect(1);
+      })
+      .then(() => {
+        playCube();
+      });
+  };
+  playCube();
+});
